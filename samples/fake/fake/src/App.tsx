c7b0229e056ff515832c5b11f92a25ded68f6f6b
h.m.song <blackmsk@gmail.com>
2024-07-04 17:01:13 +0900

    fake

--- samples/fake/fake/src/App.tsx
+++ samples/fake/fake/src/App.tsx
@@ -1,70 +1,88 @@
-// // import * as D from "./data";
+// // // import * as D from "./data";
 
-// // export default function App() {
-// //   return (
-// //     <div>
-// //       <p>
-// //         {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
-// //       </p>
-// //       <img src={D.randomAvatar()} height="50" />
-// //       <img src={D.randomImage()} height="300" />
-// //     </div>
-// //   );
-// // }
+// // // export default function App() {
+// // //   return (
+// // //     <div>
+// // //       <p>
+// // //         {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+// // //       </p>
+// // //       <img src={D.randomAvatar()} height="50" />
+// // //       <img src={D.randomImage()} height="300" />
+// // //     </div>
+// // //   );
+// // // }
 
-// import { Component } from "react";
-// import ClassComponent from "./ClassComponent";
-// import ArrowComponent from "./ArrowComponent";
+// // import { Component } from "react";
+// // import ClassComponent from "./ClassComponent";
+// // import ArrowComponent from "./ArrowComponent";
 
-// // export default function App(){
-// //   return <div>Hello function-keyword component!</div>
-// // }
+// // // export default function App(){
+// // //   return <div>Hello function-keyword component!</div>
+// // // }
 
+// // // const App = () => {
+// // //   return (
+// // //     <ul>
+// // //       <ClassComponent href="http://www.google.com" text="go to google" />
+// // //       <ArrowComponent href="http://www.X.com" text="go to X(twitter)" />
+// // //     </ul>
+// // //   );
+// // // };
 // // const App = () => {
-// //   return (
-// //     <ul>
-// //       <ClassComponent href="http://www.google.com" text="go to google" />
-// //       <ArrowComponent href="http://www.X.com" text="go to X(twitter)" />
-// //     </ul>
-// //   );
+// //   // refactoring map 함수 사용
+// //   // const texts = [<p key="1">hello</p>, <p key="2">world</p>];
+// //   const texts = ["hello", "world"].map((item, index) => (
+// //     <p key={index} children={item} />
+// //   ));
+// //   return <div children={texts} />;
 // // };
-// const App = () => {
-//   // refactoring map 함수 사용
-//   // const texts = [<p key="1">hello</p>, <p key="2">world</p>];
-//   const texts = ["hello", "world"].map((item, index) => (
-//     <p key={index} children={item} />
-//   ));
-//   return <div children={texts} />;
-// };
 
-// export default App;
+// // export default App;
 
-import EventLisner from "./pages/EventListner";
-import OnClick from "./pages/OnClick";
-import ReactOnClick from "./pages/ReactOnClick";
-import DispatchEvent from "./pages/DispatchEvent";
-import EventBubbling from "./pages/EventBubbling";
-import StopPropagation from "./pages/StopPropagation";
-import VariousInputs from "./pages/VariousInputs";
-import OnChange from "./pages/OnChange";
-import FileInput from "./pages/FileInput";
-import DragDrop from "./pages/DragDrop";
-import FileDrop from "./pages/FileDrop";
+// import EventLisner from "./pages/EventListner";
+// import OnClick from "./pages/OnClick";
+// import ReactOnClick from "./pages/ReactOnClick";
+// import DispatchEvent from "./pages/DispatchEvent";
+// import EventBubbling from "./pages/EventBubbling";
+// import StopPropagation from "./pages/StopPropagation";
+// import VariousInputs from "./pages/VariousInputs";
+// import OnChange from "./pages/OnChange";
+// import FileInput from "./pages/FileInput";
+// import DragDrop from "./pages/DragDrop";
+// import FileDrop from "./pages/FileDrop";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <FileDrop />
+//       <DragDrop />
+//       <FileInput />
+//       <OnChange />
+//       <VariousInputs />
+//       <StopPropagation />
+//       <EventBubbling />
+//       <DispatchEvent />
+//       <ReactOnClick />
+//       <OnClick />
+//       <EventLisner />
+//     </div>
+//   );
+// }
+
+import Bootstrap from "./pages/Bootstrap";
+import Icon from "./pages/Icon";
+// import Style from "./pages/Style";
+// import UsingIcon from "./pages/UsingIcon";
+// import UsingIconWithCSSClass from "./pages/UsingIconwithCSSClass";
 
 export default function App() {
   return (
     <div>
-      <FileDrop />
-      <DragDrop />
-      <FileInput />
-      <OnChange />
-      <VariousInputs />
-      <StopPropagation />
-      <EventBubbling />
-      <DispatchEvent />
-      <ReactOnClick />
-      <OnClick />
-      <EventLisner />
+      {/* <UsingIconWithCSSClass />
+      <UsingIcon />
+      <Style /> */}
+      <Icon />
+      <Bootstrap />
     </div>
   );
 }
--- samples/fake/fake/src/pages/StopPropagation.tsx
+++ samples/fake/fake/src/pages/StopPropagation.tsx
@@ -1,3 +1,20 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>;
+import type { SyntheticEvent } from "react";
+
+export default function EventBubbling() {
+  const onDivClick = (e: SyntheticEvent) =>
+    console.log("click event bubbles on <div>");
+
+  const onButtonClick = (e: SyntheticEvent) => {
+    console.log("mouseclick occurs on <button> and call stopPropagation");
+    e.stopPropagation();
+  };
+
+  return (
+    <div onClick={onDivClick}>
+      <p>StopPropagation</p>
+      <button onClick={onButtonClick}>
+        Click Me and Stop event Propagation
+      </button>
+    </div>
+  );
 }
