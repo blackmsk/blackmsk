6ebd472531cb4f29c4c003190df7063f10adff41
h.m.song <blackmsk@gmail.com>
2024-07-10 18:19:06 +0900

    day9

--- day9/hooks/src/UseContextApp1.tsx
+++ day9/hooks/src/UseContextApp1.tsx
@@ -1,23 +1,28 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Page from "./components/Page";
 import { ThemeContext } from "./context/ThemeContext";
 import { UserContext } from "./context/UserContext";
 
-
 function UseContextApp1() {
   const [isDark, setIsDark] = useState(false);
   const [userIP, setUserIP] = useState("Loading IP...");
+  const [userCountry, setCountry] = useState(" Country...");
+  const [userCity, setCity] = useState(" City...");
 
   useEffect(() => {
     const fetchIP = async () => {
       try {
-        const response = await fetch('https://api.ipify.org?format=json');
+        const response = await fetch("https://ipwhois.app/json/");
         const data = await response.json();
         setUserIP(data.ip);
+        setCountry(data.country_code);
+        setCity(data.city);
       } catch (error) {
-        console.error('Error fetching IP:', error);
-        setUserIP('Failed to load IP');
+        console.error("Error fetching IP:", error);
+        setUserIP("Failed to load IP");
+        setCountry("Failed to load Country");
+        setCity("Failed to load City");
       }
     };
 
@@ -25,7 +30,7 @@ function UseContextApp1() {
   }, []);
 
   return (
-    <UserContext.Provider value={userIP}>
+    <UserContext.Provider value={[userIP, userCity, userCountry ]}>
       <ThemeContext.Provider value={{ isDark, setIsDark }}>
         <Page />
       </ThemeContext.Provider>
